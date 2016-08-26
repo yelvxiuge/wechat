e717bff9d308ea60679de5139189d55b8e3de747
sukhoi <sukhoi27i@gmail.com>
2016-08-26 20:15:35 +0800

    test

--- web/oauth_callback.php
+++ web/oauth_callback.php
@@ -2,9 +2,20 @@
 require '../vendor/autoload.php';
 
 use EasyWeChat\Foundation\Application;
-$config = [
+
+
+    $config = [
+        // ...
+        'app_id'     =>'wxb84db8bb472503ed',
+        'appsecret' =>'d4624c36b6795d1d99dcf0547af5443d',
+        'token'     =>'Asdsd',
+        'oauth' => [
+            'scopes'   => ['snsapi_userinfo'],
+            'callback' => '/oauth_callback.php',
+        ],
+        // ..
+    ];
     // ...
-];
 $app = new Application($config);
 $oauth = $app->oauth;
 // 获取 OAuth 授权结果用户信息
